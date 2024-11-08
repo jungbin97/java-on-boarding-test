3420c10f24281c266aaecf4c0186283eb9e061cb
jungbin97 <ljb5054@gmail.com>
2024-11-08 15:50:09 +0900

    feat: swagger config 설

---
+++ src/main/java/com/example/onboarding/common/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.example.onboarding.common.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @io.swagger.v3.oas.annotations.info.Info(title = "onboarding", version = "1.0.0", description ="백엔드 온보딩 과제 입니다.")
+)
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("bearer-jwt", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .addSecurityItem(new SecurityRequirement().addList("bearer-jwt"));
+    }
+}
